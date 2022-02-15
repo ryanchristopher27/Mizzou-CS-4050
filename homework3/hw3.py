1d59e3ce08c49db186318649831a89509cad1fdd
ryanchristopher27 <ryandchristopher27@gmail.com>
2022-02-14 21:48:33 -0600

    worked on and nearly completed homework3
    worked on homework3 and completed it for the most part

---
+++ homework3/hw3.py
@@ -0,0 +1,74 @@
+# CS 4050
+# Homework 3
+# Ryan Christopher
+
+# Imports
+from timeit import default_timer as time
+import sys
+
+# Functions
+def readFile(filename):
+    f = open(filename, "r")
+    mergedList = []
+    for msg in f.readlines():
+        msg = msg.strip('\n')
+        adm = msg.split(' ')
+        mergedList.append(adm)
+    return mergedList
+
+def BFS(sID, list, nNode):
+    visited = [False] * nNode
+    level = [0] * nNode
+    q = [sID]
+    count = 0
+
+    visited[sID] = True
+    level[sID] = 0
+
+    while q:
+        vis = q[0]
+
+        print(vis)
+
+        q.pop(0)
+
+        for i in range(nNode):
+            # print(list[vis][i])
+            # print(visited[i])
+            if ((int(list[vis][i]) == 1) and (not visited[i])):
+                print("found: ", i)
+                q.append(i)
+
+                visited[i] = True
+                level[i] = level[vis] + 1
+                count = level[i]
+
+    print("count: ", count)
+
+    for i in range(nNode):
+        print(" ", i, " -->", level[i])
+
+    for i in range(count+1):
+        print("L", i, ":")
+        for j in range(nNode):
+            if (level[j] == i):
+                print(j)
+        
+
+
+# Main
+def main():
+    nNode = sys.argv[1]
+    sID = sys.argv[2]
+    filename = sys.argv[3]
+
+    list = readFile(filename)
+
+    BFS(int(sID)-1, list, int(nNode))
+
+
+
+
+
+
+main()

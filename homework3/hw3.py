15bfbe28194f24f7aca3944700714d9a4a78e459
ryanchristopher27 <ryandchristopher27@gmail.com>
2022-02-20 13:42:47 -0600

    completed homework 3
    Made finishing touches to homework 3 and turned it in.

--- homework3/hw3.py
+++ homework3/hw3.py
@@ -17,62 +17,55 @@ def readFile(filename):
     return mergedList
 
 def BFS(sID, list, nNode):
+    # Initialize variables, arrays, and queues
     visited = [False] * nNode
     level = [0] * nNode
     q = [sID]
     count = 0
 
+    # Update values with starting node
     visited[sID] = True
     level[sID] = 0
 
+    # Iterate through queue until empty
     while q:
         vis = q[0]
-
-        # print(vis)
 
         q.pop(0)
 
         for i in range(nNode):
-            # print(list[vis][i])
-            # print(visited[i])
+            # Check if iterated node has edge with current node and is not visited
             if ((int(list[vis][i]) == 1) and (not visited[i])):
-                # print("found: ", i)
+                # Add new node to queue and update arrays accordingly
                 q.append(i)
-
                 visited[i] = True
                 level[i] = level[vis] + 1
                 count = level[i]
 
-    # print("count: ", count)
-
-    # for i in range(nNode):
-    #     print(" ", i, " -->", level[i])
     temp = []
+    # Iterate through number of total levels
     for i in range(count+1):
-        # print("L", i, ":")
+        # Iterate through all nodes in graph
         for j in range(nNode):
+            # Check if node has level equal to iterated level
             if (level[j] == i):
                 temp.append(str(j+1))
-                # print(j)
         print("L"+ str(i) + ": " + ', '.join(temp))
         temp.clear()
 
-
 # Main
 def main():
+    # Start Time
     startTime = time()
+    # Get Command Line Variables
     nNode = sys.argv[1]
     sID = sys.argv[2]
     filename = sys.argv[3]
-
+    # Read File
     list = readFile(filename)
-
+    # Breadth First Search
     BFS(int(sID)-1, list, int(nNode))
     endTime = time()
-    print("All Time: ", (endTime - startTime)*1000, "ms")
-
-
-
-
+    print("Execution Time: ", (endTime - startTime)*1000, "ms")
 
 main()
